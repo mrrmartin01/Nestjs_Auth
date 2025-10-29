240c14b3b13c700028909ac7b1c4faaa8f2ff0cd
mrmartin <theophilusmartin123@gmail.com>
2025-10-29 17:36:34 -0500

    Add proper cookie management and swagger docs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,13 +2,24 @@ import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 
-@UseGuards(JwtAuthGuard)
+
+//Put this on top of the controller to protect all its routes
+// @UseGuards(JwtAuthGuard)
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+// Put this on top of specific routes to protect them
+  // @UseGuards(JwtAuthGuard)
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
+
+  //Here's a public route that doesn't need protection
+  @Get('public')
+  getPublic(): string {
+    return 'This is a public route';
+  }
+
 }
